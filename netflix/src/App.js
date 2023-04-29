b755df5475f86cd4b828e251da77c599c55bd81e
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2023-04-29 04:14:27 -0300

    ;

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -16,17 +16,18 @@ export default () => {
             //pegando toda a lista
             let lista = await tmdb.getHomeList()
             setMovieList(lista)
-            
+
             //pegando o feature
-            let acao =lista.filter(i=>i.slug ==='acao')
-            let sorteio = Math.floor(Math.random()*(acao[0].itens.results.length -1))
+            let acao = lista.filter(i => i.slug === 'acao')
+            let sorteio = Math.floor(Math.random() * (acao[0].itens.results.length - 1))
             let escolhido = acao[0].itens.results[sorteio]
-            let infoEscolhido = await tmdb.getInfoFilme(escolhido.id,'movie')
+            let infoEscolhido = await tmdb.getInfoFilme(escolhido.id, 'movie')
 
-           setDadosDestaque(infoEscolhido)
-           console.log(infoEscolhido);
+            setDadosDestaque(infoEscolhido)
+            console.log(infoEscolhido);
+            console.log(infoEscolhido.overview);
 
-           
+
         }
         carregaTudo()
     }, []);
@@ -35,8 +36,8 @@ export default () => {
 
             {dadosDestaque &&
                 <FilmeDestaque
-                    item={FilmeDestaque}
-                    
+                    item={dadosDestaque}
+
                 />}
 
 
